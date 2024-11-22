d3eb53b794a29883ec79edaeda45fb29b30d3506
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2024-11-22 12:24:38 +0700

    UPDATE

---
+++ ParkingLot-Api/Entities/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingLot_Api.Model;
+
+namespace ParkingLot_Api.Entities
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ ParkingLot-Api/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingLot_Api.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public float Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
