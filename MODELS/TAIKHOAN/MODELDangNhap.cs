fab4aaf1113a6245d01dad1c2e5b37b7e7d1f6c7
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2024-11-26 17:06:40 +0700

    UPDATE

---
+++ MODELS/TAIKHOAN/MODELDangNhap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELS.TAIKHOAN
+{
+    public class MODELDangNhap
+    {
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [DataType(DataType.Password)]
+        public string MatKhau { get; set; }
+        [Display(Name = "Ghi nhớ tôi?")]
+        public bool GhiNhoToi { get; set; }
+    }
+}
