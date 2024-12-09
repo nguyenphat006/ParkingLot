38a202c538a5061b7b14e38957ef5901e760a29d
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2024-12-09 11:39:53 +0700

    UPDATE

--- src/app/parkinglot/page.tsx
+++ src/app/parkinglot/page.tsx
@@ -2,7 +2,7 @@ import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import TableOne from "@/components/Tables/TableOne";
 import TableThree from "@/components/Tables/TableThree";
 import TableTwo from "@/components/Tables/TableTwo";
-// import IndexParkingLot from "@/components/ViewParkingLot/Index"
+import IndexParkingLot from "@/components/ViewParkingLot/Index"
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLaout";
 
@@ -15,9 +15,8 @@ const ParkingLot = () => {
   return (
     <DefaultLayout>
       <Breadcrumb pageName="ParkingLot" />
-
       <div className="flex flex-col gap-10">
-     
+        <IndexParkingLot />
       </div>
     </DefaultLayout>
   );
