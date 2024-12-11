3811756b127e540f8bb56c50c82d452c68b4336e
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2024-12-11 15:56:12 +0700

    UPDATE

--- src/app/parkinglot/page.tsx
+++ src/app/parkinglot/page.tsx
@@ -1,7 +1,4 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import TableOne from "@/components/Tables/TableOne";
-import TableThree from "@/components/Tables/TableThree";
-import TableTwo from "@/components/Tables/TableTwo";
 import IndexParkingLot from "@/components/ViewParkingLot/Index"
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLaout";
--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import ChartThree from "../Charts/ChartThree";
 import ChartTwo from "../Charts/ChartTwo";
-import ChatCard from "../Chat/ChatCard";
-import TableOne from "../Tables/TableOne";
 import MapOne from "../Maps/MapOne";
 import DataStatsOne from "@/components/DataStats/DataStatsOne";
 import ChartOne from "@/components/Charts/ChartOne";
@@ -17,11 +15,7 @@ const ECommerce: React.FC = () => {
         <ChartOne />
         <ChartTwo />
         <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
-        <ChatCard />
+        <MapOne />       
       </div>
     </>
   );
